0c3c36f168766f09c6b6407914f763ec435f2e2b
Evan Bond <evanbond24@gmail.com>
2024-04-19 15:41:37 +0300

    feat (button): add styling for  secondary buttons

--- app/buttons/page.tsx
+++ app/buttons/page.tsx
@@ -6,6 +6,8 @@ const ButtonsPage = () => {
       <Button>Default</Button>
       <Button variant={'primary'}>Primary</Button>
       <Button variant={'primaryOutline'}>Primary Outline</Button>
+      <Button variant={'secondary'}>Secondary</Button>
+      <Button variant={'secondaryOutline'}>Secondary Outline</Button>
     </div>
   );
 };
