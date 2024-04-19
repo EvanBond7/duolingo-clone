14320ad661f1ebf0f4b4afeb098c41cdf40b4038
Evan Bond <evanbond24@gmail.com>
2024-04-19 16:05:27 +0300

    feat (button): add danger styling for buttons

--- app/buttons/page.tsx
+++ app/buttons/page.tsx
@@ -8,6 +8,8 @@ const ButtonsPage = () => {
       <Button variant={'primaryOutline'}>Primary Outline</Button>
       <Button variant={'secondary'}>Secondary</Button>
       <Button variant={'secondaryOutline'}>Secondary Outline</Button>
+      <Button variant={'danger'}>Danger</Button>
+      <Button variant={'dangerOutline'}>Danger Outline</Button>
     </div>
   );
 };
