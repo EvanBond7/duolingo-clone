ee73ce2738282a759ddc2771f3f609dbf827e07d
Evan Bond <evanbond24@gmail.com>
2024-04-21 13:44:17 +0300

    feat: create a StickyWrapper component

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,12 @@
+import { StickyWrapper } from '@/components/sticky-wrapper';
 import React from 'react';
 
 const LearnPage = () => {
-  return <div>Learn Page</div>;
+  return (
+    <div className='flex flex-row-reverse gap-[48px] px-6'>
+      <StickyWrapper>My sticky sidebar</StickyWrapper>
+    </div>
+  );
 };
 
 export default LearnPage;
