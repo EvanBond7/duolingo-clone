c7cedc78a6337a1eb9d511cf8fd1cad6b1cc9d50
Evan Bond <evanbond24@gmail.com>
2024-04-27 15:58:40 +0300

    feat: sounds for challenges

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -9,6 +9,7 @@ import { Footer } from './footer';
 import { upsertChallengeProgress } from '@/actions/challenge-progress';
 import { toast } from 'sonner';
 import { reduceHearts } from '@/actions/user-progress';
+import { useAudio } from 'react-use';
 
 type Props = {
   initialPercentage: number;
@@ -28,6 +29,10 @@ export const Quiz = ({
   initialLessonChallenges,
   userSubscription,
 }: Props) => {
+  const [correctAudio, _c, correctControls] = useAudio({ src: '/correct.wav' });
+  const [incorrectAudio, _ic, incorrectControls] = useAudio({
+    src: '/incorrect.wav',
+  });
   const [pending, startTransition] = useTransition();
 
   const [hearts, setHearts] = useState(initialHearts);
@@ -87,6 +92,7 @@ export const Quiz = ({
               return;
             }
 
+            correctControls.play();
             setStatus('correct');
             setPercentage((prev) => prev + 100 / challenges.length);
 
@@ -105,6 +111,7 @@ export const Quiz = ({
               return;
             }
 
+            incorrectControls.play();
             setStatus('wrong');
 
             if (!response?.error) {
@@ -122,6 +129,8 @@ export const Quiz = ({
       : challenge.question;
   return (
     <>
+      {incorrectAudio}
+      {correctAudio}
       <Header
         hearts={hearts}
         percentage={percentage}
