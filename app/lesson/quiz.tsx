532d14f4aca6a514e58cdb4591e493d143ad8b1c
Evan Bond <evanbond24@gmail.com>
2024-04-28 15:38:25 +0300

    feat: heartModal component for the challenge

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -14,6 +14,7 @@ import { useAudio, useWindowSize } from 'react-use';
 import Image from 'next/image';
 import { ResultCard } from './result-card';
 import { useRouter } from 'next/navigation';
+import { useHeartsModal } from '@/store/use-hearts-modal';
 
 type Props = {
   initialPercentage: number;
@@ -33,6 +34,8 @@ export const Quiz = ({
   initialLessonChallenges,
   userSubscription,
 }: Props) => {
+  const { open: openHeartsModal } = useHeartsModal();
+
   const { width, height } = useWindowSize();
   const router = useRouter();
   const [finishAudio, _a] = useAudio({ src: '/finish.mp3', autoPlay: true });
@@ -97,7 +100,7 @@ export const Quiz = ({
         upsertChallengeProgress(challenge.id)
           .then((response) => {
             if (response?.error === 'hearts') {
-              console.error('Missing hearts');
+              openHeartsModal();
               return;
             }
 
@@ -116,7 +119,7 @@ export const Quiz = ({
         reduceHearts(challenge.id)
           .then((response) => {
             if (response?.error === 'hearts') {
-              console.error('Missing hearts');
+              openHeartsModal();
               return;
             }
 
