2afb65cc438d1e32d92a31c3ba93115bb50a5c4a
Evan Bond <evanbond24@gmail.com>
2024-04-29 21:51:20 +0300

    fix: userSubscription working in the challenge progress

--- actions/user-progress.ts
+++ actions/user-progress.ts
@@ -1,7 +1,11 @@
 'use server';
 
 import db from '@/db/drizzle';
-import { getCourseById, getUserProgress } from '@/db/queries';
+import {
+  getCourseById,
+  getUserProgress,
+  getUserSubscription,
+} from '@/db/queries';
 import { challengeProgress, challenges, userProgress } from '@/db/schema';
 import { auth, currentUser } from '@clerk/nextjs/server';
 import { and, eq } from 'drizzle-orm';
@@ -64,7 +68,7 @@ export const reduceHearts = async (challengeId: number) => {
 
   const currentUserProgress = await getUserProgress();
 
-  // TODO Get user subscription
+  const userSubscription = await getUserSubscription();
 
   const challenge = await db.query.challenges.findFirst({
     where: eq(challenges.id, challengeId),
@@ -93,7 +97,11 @@ export const reduceHearts = async (challengeId: number) => {
     throw new Error('User progress not found');
   }
 
-  // TODO Handle subscription
+  if (userSubscription?.isActive) {
+    return {
+      error: 'subscription',
+    };
+  }
 
   if (currentUserProgress.hearts === 0) {
     return { error: 'hearts' };
