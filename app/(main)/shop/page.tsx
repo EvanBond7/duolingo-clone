bef4c38817fd6a13ee93378b6303362cbc18ca2a
Evan Bond <evanbond24@gmail.com>
2024-04-29 16:16:40 +0300

    feat: refill hearts with points

--- app/(main)/shop/page.tsx
+++ app/(main)/shop/page.tsx
@@ -4,6 +4,7 @@ import { UserProgress } from '@/components/user-progress';
 import { getUserProgress } from '@/db/queries';
 import Image from 'next/image';
 import { redirect } from 'next/navigation';
+import { Items } from './items';
 
 const ShopPage = async () => {
   const userProgressData = getUserProgress();
@@ -33,6 +34,11 @@ const ShopPage = async () => {
           <p className='text-muted-foreground text-center text-lg mb-6'>
             Spend your points on cool stuff.
           </p>
+          <Items
+            hearts={userProgress.hearts}
+            points={userProgress.points}
+            hasActiveSubscription={false} // TODO add subscription
+          />
         </div>
       </FeedWrapper>
     </div>
